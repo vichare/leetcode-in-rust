43910d53eb22c56cc3be85823028704f65ed6c5d
Dolphin163 <vicharewang@gmail.com>
2021-08-13 15:36:15 +1000

    More test cases.

--- shuffle-string/src/lib.rs
+++ shuffle-string/src/lib.rs
@@ -17,9 +17,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sample() {
+    fn example_1() {
         let result = Solution::restore_string(
-            "codeleet".to_string(), vec![4,5,6,7,0,2,1,3]);
+            "codeleet".to_string(), vec![4, 5, 6, 7, 0, 2, 1, 3]);
         assert_eq!(result, "leetcode".to_string());
+    }
+
+    #[test]
+    fn example_2() {
+        let result = Solution::restore_string(
+            "abc".to_string(), vec![0, 1, 2]);
+        assert_eq!(result, "abc".to_string());
+    }
+
+    #[test]
+    fn example_3() {
+        let result = Solution::restore_string(
+            "aiohn".to_string(), vec![3, 1, 4, 2, 0]);
+        assert_eq!(result, "nihao".to_string());
+    }
+
+    #[test]
+    fn example_4() {
+        let result = Solution::restore_string(
+            "aaiougrt".to_string(),  vec![4, 0, 2, 6, 7, 3, 1, 5]);
+        assert_eq!(result, "arigatou".to_string());
+    }
+
+    #[test]
+    fn example_5() {
+        let result = Solution::restore_string(
+            "art".to_string(), vec![1, 0, 2]);
+        assert_eq!(result, "rat".to_string());
     }
 }

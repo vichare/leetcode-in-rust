30c562e05229647b58627fd7e12bd98a5cc0fa0a
Dolphin163 <vicharewang@gmail.com>
2021-08-17 22:15:37 +1000

    Add solution for problem find-center-of-star-graph.

--- new_problem.sh
+++ new_problem.sh
@@ -5,3 +5,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cargo new $PROBLEM --lib
 sed -i '1s#^#//! https://leetcode.com/problems/shuffle-string/\n\npub struct Solution {}\n\nimpl Solution {\n}\n\n#' $SCRIPT_DIR/$PROBLEM/src/lib.rs
+echo "vim ${PROBLEM}/src/lib.rs"
+echo "git add ${PROBLEM} && git commit -m \"Add solution for problem ${PROBLEM}.\" && git push"
